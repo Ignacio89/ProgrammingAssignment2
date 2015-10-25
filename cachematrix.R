7e22b7e5f074d2b5485bb24bfa6c1e1778d12a42
Ignacio89 <cigl.carlos@gmail.com>
2015-10-25 11:07:41 -0600

    Assigment 2 update
    Assigment 2, cache of inverted matrix. Extremely loyal to the example functions of the cache of mean, with minor changes so it fits the inverted matrix process.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are part of the assigment 2 of Coursera's course 
+## "R Programming" course. They serve the purpose of using lexical 
+## scoping to avoid recalculate costly values, in this particular
+##case, the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a list of functions that have the
+## purpose of setting and getting de values for a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  InvMat <- NULL
+  set <- function(y) {
+    x <<- y
+    InvMat <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) InvMat <<- inverse
+  getinverse <- function() InvMat
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve function takes the list of functions from makeCacheMatrix
+## and with its help, either calculates the inverse and chache it, or
+## gets the cached inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of a matrix
+  InvMat <- x$getinverse()
+  if(!is.null(InvMat)) {
+    message("getting cached data")
+    return(InvMat)
+  }
+  data <- x$get()
+  InvMat <- solve(data)
+  x$setinverse(InvMat)
+  InvMat
 }
